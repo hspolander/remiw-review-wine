5eb32e5a9f361c7acc44a29c6edd4b8d46f55530
Henrik Spolander <henrik.spolander@kaustik.com>
2022-11-09 21:05:33 +0100

    Add count to filters in sys search

--- app/routes/review.tsx
+++ app/routes/review.tsx
@@ -2,10 +2,13 @@ import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, Outlet, useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
+import natsort from 'natsort'
+import { prisma } from "@prisma/client";
+
 
 type LoaderData = {
-  countries: Array<{ country: string }>;
-  colors: Array<{ categoryLevel2: string }>;
+  countriesWithCount: { country: string, count: number }[];
+  colorsWithCount: { categoryLevel2: string, count: number }[];
   searchParams?: {
     categoryLevel2?: string[];
     country?: string[];
@@ -14,28 +17,96 @@ type LoaderData = {
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const countries = await db.systembolagetWine.findMany({
-    select: { country: true },
-    distinct: ["country"],
-    orderBy: { country: "asc" },
-  });
-  const colors = await db.systembolagetWine.findMany({
-    select: { categoryLevel2: true },
-    distinct: ["categoryLevel2"],
-    orderBy: { categoryLevel2: "asc" },
-  });
-  
-  //Lägg till grapes
-  //Lägg till smakklockor
+  var sorter = natsort();
 
   const url = new URL(request.url);
   const categoryLevel2 = url.searchParams.getAll("categoryLevel2") || [];
   const country = url.searchParams.getAll("country") || [];
   const name = url.searchParams.get("name") || "";
 
+  const countries = await db.systembolagetWine.findMany({
+    select: { country: true },
+    distinct: ["country"],
+    orderBy: { country: "asc" },
+  });
+
+  const colors = await db.systembolagetWine.findMany({
+    select: { categoryLevel2: true,},
+    distinct: ["categoryLevel2"],
+    orderBy: { categoryLevel2: "asc" },
+  });
+
+  const colorsByParams = await db.systembolagetWine.groupBy({
+    by: ['categoryLevel2'],
+    where: {
+      AND: [
+        { ...(categoryLevel2?.length > 0
+          ? { categoryLevel2: { in: categoryLevel2 } }
+          : {}
+        )},
+        { ...(country?.length > 0
+          ? { country: { in: country } }
+          : {}
+        )},
+        {
+          OR: [
+            { productNameBold: { contains: name } },
+            { productNameThin: { contains: name } },
+          ],
+        },
+      ],
+    },
+    _count: {
+      categoryLevel2: true,
+    },
+    orderBy: {
+      _count: {
+        categoryLevel2: 'desc',
+      },
+    },  
+  });
+
+  const countriesByParams = await db.systembolagetWine.groupBy({
+    by: ['country'],
+    where: {
+      AND: [
+        { ...(categoryLevel2?.length > 0
+          ? { categoryLevel2: { in: categoryLevel2 } }
+          : {}
+          )},
+          { ...(country?.length > 0
+            ? { country: { in: country } }
+        : {}
+        )},
+        {
+          OR: [
+            { productNameBold: { contains: name } },
+            { productNameThin: { contains: name } },
+          ],
+        },
+      ],
+    },
+    _count: {
+      country: true,
+    },
+    orderBy: {
+      _count: {
+        country: 'desc',
+      },
+    },  
+  });
+
+  const countriesWithCount = countries.map(({country}) => {
+    const count = countriesByParams.find((item) => item.country === country ? item._count.country : 0)
+    return { country, count: count?._count.country ?? 0}
+  }).sort((a, b) => sorter(b.count, a.count))
+  const colorsWithCount = colors.map(({categoryLevel2}) => {
+    const count = colorsByParams.find((item) => item.categoryLevel2 === categoryLevel2 ? item._count.categoryLevel2 : 0)
+    return { categoryLevel2, count: count?._count.categoryLevel2 ?? 0}
+  }).sort((a, b) => sorter(b.count, a.count))
   const data: LoaderData = {
-    countries,
-    colors,
+    colorsWithCount,
+    countriesWithCount,
     searchParams: {
       categoryLevel2,
       country,
@@ -48,6 +119,7 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function ReviewRoute() {
   const loaderData = useLoaderData<LoaderData>();
+
   return (
     <div className="flex flex-column bg-white dark:bg-gray-900">
       <div className="basis-1/4 mr-6">
@@ -57,17 +129,17 @@ export default function ReviewRoute() {
             <div className="w-full max-w-xs">
               <label className="label">
                 <span className="label-text">Name</span>
-                <input type="text" name="name" className="input input-bordered w-full max-w-xs" defaultValue={loaderData.searchParams?.name} placeholder="21 Gables" />
               </label>
+                <input type="text" name="name" className="input input-bordered w-full max-w-xs" defaultValue={loaderData.searchParams?.name} placeholder="21 Gables" />
             </div>
           </div>
           <div className="divider"></div> 
           <div>
             <h1 className="text-xl">Country</h1>
-            {loaderData.countries.map(({ country }) => (
+            {loaderData.countriesWithCount.map(({ country, count }) => (
               <div key={country}>
                 <label className="label cursor-pointer">
-                  <span className="label-text">{country}</span> 
+                <span className="label-text">{country}</span><div className="w-full text-right text-slate-400 pr-2">{count}</div>
                   <input name="country" value={country} key={country} type="checkbox" defaultChecked={loaderData.searchParams?.country?.some((urlParamCountry) => urlParamCountry === country)}  className="checkbox" />
                 </label>
               </div>
@@ -76,10 +148,10 @@ export default function ReviewRoute() {
           <div className="divider"></div> 
           <div>
             <h1 className="text-xl">Type</h1>
-            {loaderData.colors.map(({ categoryLevel2 }) => (
+            {loaderData.colorsWithCount.map(({ categoryLevel2, count }) => (
               <div key={categoryLevel2}>
                 <label className="label cursor-pointer">
-                  <span className="label-text">{categoryLevel2}</span> 
+                  <span className="label-text">{categoryLevel2}</span><div className="w-full text-right text-slate-400 pr-2">{count}</div>
                   <input name="categoryLevel2" value={categoryLevel2} key={categoryLevel2} defaultChecked={loaderData.searchParams?.categoryLevel2?.some((urlParamCategoryLevel2) => urlParamCategoryLevel2 === categoryLevel2)} type="checkbox" className="checkbox" />
                 </label>
               </div>
